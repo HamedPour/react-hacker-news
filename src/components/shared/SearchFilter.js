5a40c2ab952027aeb696781122948b0bba938b5e
HamedPour <hamed_k.pour@hotmail.com>
2020-06-08 17:27:34 +0100

    Styled-component restructure required

--- src/components/shared/SearchFilter.js
+++ src/components/shared/SearchFilter.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
 const SearchFilter = () => {
@@ -8,20 +8,18 @@ const SearchFilter = () => {
     min-height: 40px;
     align-items: center;
     border: 2px solid blue;
+    text-selection: none;
 
     & span {
+      font-size: 0.8rem;
       padding: 0 5px;
     }
   `;
   const StoriesFilter = styled.ul`
-    border: 1px solid red;
-  `;
-
-  const DropItem = styled.li`
-    list-style: none;
-    & button {
-      color: red;
-    }
+    padding: 3px 5px 3px 5px;
+    border: 1px solid rgba(0, 0, 0, 0.5);
+    cursor: pointer;
+    position: relative;
   `;
 
   const PopularityFilter = styled.ul`
@@ -37,16 +35,39 @@ const SearchFilter = () => {
     display: none;
   `;
 
+  const DropDown = styled.div`
+    border: 1px solid rgba(0, 0, 0, 0.2);
+    min-width: 100px;
+    list-style: none;
+    background-color: #fff;
+    position: absolute;
+    top: 40px;
+    & li {
+      padding: 10px 5px 10px 10px;
+      border-bottom: 1px solid rgba(0, 0, 0, 0.2);
+      cursor: pointer;
+    }
+  `;
+
+  const [storyFilter, setStoryFilter] = useState([
+    "All",
+    "Stories",
+    "Comments",
+  ]);
+
+  function dropDownMenu() {
+    console.log("click");
+  }
+
   return (
     <>
       <MainContainer>
         <span>Search</span>
         <StoriesFilter>
-          <DropItem>
-            <button href="#">All</button>
-            <button href="#">Stories</button>
-            <button href="#">Comments</button>
-          </DropItem>
+          <span>
+            {storyFilter[0]} <i className="fa fa-angle-down" />
+          </span>
+          <DropDown>Drop Down items goes here</DropDown>
         </StoriesFilter>
         <span>by</span>
         <PopularityFilter>DROPDOWN</PopularityFilter>
